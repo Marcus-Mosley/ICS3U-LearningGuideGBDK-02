a4ed9556506dd7e9e2d2bd35248a4e8b3a33be0f
Ubuntu <ubuntu@ip-172-31-95-19.ec2.internal>
2020-12-18 16:13:29 +0000

    Added in game.gb

---
+++ game.c
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Marcus A. Mosley All rights reserved.
+//
+// Created by Marcus A. Mosley
+// Created on December 2020
+// This program displays a sprite on a Gameboy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+#include "smiler.c"
+
+int main() {
+    // this function displays a happy face
+
+    set_sprite_data(0, 2, smiler);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
+}
