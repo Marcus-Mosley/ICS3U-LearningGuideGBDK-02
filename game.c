ef5feb0cbd94a57234db6ff3a0665ee70a6b950a
Marcus-Mosley <68971666+Marcus-Mosley@users.noreply.github.com>
2020-12-18 11:15:28 -0500

    Added in game.c

--- game.c
+++ game.c
@@ -2,7 +2,7 @@
 //
 // Created by Marcus A. Mosley
 // Created on December 2020
-// This program displays a sprite on a Gameboy
+// This program displays a sprite on a GameBoy
 
 #include <stdio.h>
 #include <gb/gb.h>
